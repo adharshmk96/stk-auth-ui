d28e907fa442cf111e3d169bd4626b0446f1a655
Adharsh Manikandan <adharsh.manikandan@agoda.com>
2023-07-15 10:42:12 +0700

    chore: user list update

--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,57 +1,69 @@
-import { Component, ComponentProps, Resource, Suspense, createResource } from 'solid-js';
+import { AUTH_SERVER_URL } from "@/config";
+import DashboardLayout from "@/layouts/DashboardLayout";
+import { Component, ComponentProps, Resource, Suspense, createResource } from "solid-js";
 
 interface UserListProps extends ComponentProps<any> {
-    // add props here
+  // add props here
 }
 
 type UserData = {
-    id: number;
-    name: string;
-}
+  id: number;
+  name: string;
+  email: string;
+  created_at: string;
+  updated_at: string;
+};
 
-const fetchUserList = async () => {
-    // sleep for 1 second
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return [
-        { id: 1, name: 'John Doe' },
-        { id: 2, name: 'Jane Doe' },
-        { id: 3, name: 'John Smith' },
-    ]
-}
+// const fetchUserList = async () => {
+//     await fetch(`${AUTH_SERVER_URL}/api/admin/users`, {
+//         method: "GET",
+//         credentials: "include"
+//     }).json();
+  
+// };
 
+const fetchUserList = async () => (
+    await fetch(`${AUTH_SERVER_URL}/api/admin/users`, {
+            method: "GET",
+            credentials: "include"
+        })).json();
+
+        
 type UserListTableProps = {
-    userList: Resource<UserData[]>;
-}
+  userList: Resource<UserData[]>;
+};
 
-const UserListTable: Component<UserListTableProps> = (props) => {
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.userList()?.map((user) => (
-                    <tr>
-                        <td>{user.id}</td>
-                        <td>{user.name}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    )
-}
+const UserListTable: Component<UserListTableProps> = props => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.userList()?.map(user => (
+          <tr>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 const UserList: Component<UserListProps> = () => {
-    const [userList, {refetch}] = createResource<UserData[]>(fetchUserList);
+  const [userList, { refetch }] = createResource<UserData[]>(fetchUserList);
 
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <UserListTable userList={userList} />
-        </Suspense>
-    )
-}
+  return (
+    <DashboardLayout>
+      <Suspense fallback={<div>Loading...</div>}>
+        <UserListTable userList={userList} />
+      </Suspense>
+    </DashboardLayout>
+  );
+};
 
 export default UserList;
